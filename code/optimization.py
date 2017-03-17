c2964014f17550f051235d678c6a56152ff51c74
Nilesh <nilesh5760@gmail.com>
2017-03-16 23:12:03 -0400

    Update radme file

--- code/optimization.py
+++ code/optimization.py
@@ -42,9 +42,9 @@ class SGD(object):
         # TODO: Backpropagation code
         dv_Ws = []
         dv_bs = []
-        
+
         x_l = [x.copy()]
-        y_pred = Network(graph).inference(x)        
+        y_pred = Network(graph).inference(x)
         error = loss.forward(y_pred,y)
         dv_o = loss.backward(y_pred,y)
         n_layer = len(graph.layers)
@@ -53,14 +53,14 @@ class SGD(object):
         for layer_number in stage:
             layer = graph.layers[layer_number]
             x_l = x_l + [layer.forward(x_l[layer_number])]
-            
+
         for layer_number in stage[::-1]:
             layer = graph.layers[layer_number]
             dv  = layer.backward(x=x_l[layer_number],dv_y=dv_o)
             dv_o = dv[0]
             dv_Ws = [dv[1]]+dv_Ws
             dv_bs = [dv[2]]+dv_bs
-            
+
         out = [dv_Ws,dv_bs]
         return(out)
 
@@ -91,22 +91,22 @@ class SGD(object):
             for k in xrange(0, n, self.batch_size)
         ]
 
-        # TODO: SGD code        
+        # TODO: SGD code
         for batch in batches:
-            
+
             dv_Ws=[]
             dv_bs=[]
-            
+
             for x,y in batch:
                 dv_Wi,dv_bi = self.compute_gradient(x=x,y=y,graph=graph,loss=loss)
                 dv_Ws += [dv_Wi]
                 dv_bs += [dv_bi]
-            
+
             dv_Wsum = np.sum(dv_Ws,axis=0)
             dv_bsum = np.sum(dv_bs,axis=0)
             n_layer = len(graph.layers)
-            
+
             for layer_number in np.arange(n_layer):
                 if graph.layers[layer_number].W is not None:
-                    graph.layers[layer_number].W -= (self.learning_rate*dv_Wsum[layer_number])/batch_size
-                    graph.layers[layer_number].b -= (self.learning_rate*dv_bsum[layer_number])/batch_size
+                    graph.layers[layer_number].W -= (self.learning_rate*dv_Wsum[layer_number])/self.batch_size
+                    graph.layers[layer_number].b -= (self.learning_rate*dv_bsum[layer_number])/self.batch_size
---
+++ readme.md
@@ -0,0 +1,18 @@
+### Structure :
+
+The repository contains code for fully connected neural network & also a demo.py file used to define & run the network. A decent variety of tests are implemented and can be seen in the solution.ipynb notebook(end). The network is designed to classify MNIST digit recognition dataset.
+
+ * ./code folder:
+   - contains original files with implemented classes
+   - Added tests_all.py file which contains code used for creating plots & testing variations in the parameters
+ * report.pdf:
+   - Contains report for the network performance on different parameters
+ * Solution.ipynb contains the full standalone work :
+   - Fully implemented classes
+   - Demo run
+   - Testing code
+   - variation tests
+   - Report generation python code & markdown
+
+* Solution.html is just the html version for this.
+* I have added my results.pkl file, which is essentially the object where I have stored results from testing.
